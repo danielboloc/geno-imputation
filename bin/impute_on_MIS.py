41274c9f2f1dbbbd3706d6b98782f86fa1bc04e6
Daniel Boloc <danielboloc@gmail.com>
2022-08-08 21:55:07 +0200

    style: add useful comments

--- bin/impute_on_MIS.py
+++ bin/impute_on_MIS.py
@@ -26,52 +26,51 @@ from random import random
               help="API path in the server")
 @click.option('-pt','--patient',
               help="Patient name")
-def main(file,
-         url,
-         token,
-         password,
-         api,
-         patient):
+def main(file, url, token, password, api, patient):
+    """
 
+    """
+
+    # open to read in  binary format
     files = [('files', open(file, 'rb'))]
 
+    # setup API request
     headers = {'X-Auth-Token' : token }
     data = {
-      'refpanel': f'apps@1000g-phase-3-v5@2.0.0',
-      'population': 'eur',
-      'build': 'hg38',
-      'password': f'{password}'
+      'refpanel': f'apps@1000g-phase-3-v5@2.0.0', # can change depending on use
+      'population': 'eur', # there are 'amr','afr','eas','mixed' 
+      'build': 'hg38', # current build
+      'password': f'{password}' # can be changed at run time
     }
 
+    # create a low integer to use for sleep 
     string_to_int = f"{patient}_{file}"
     time_to_sleep = int(str(hash(string_to_int))[1:4])/1000
-    time.sleep(time_to_sleep) # to prevent weird error of sending all chromosomes and failing
+    time.sleep(time_to_sleep) # to prevent error of sending all chromosomes and failing
+    # send request to API
     r = requests.post(url + api, files=files, data=data, headers=headers)
 
+    # retrieve info from request
     success = r.json()["success"]
     job_id = r.json()["id"]
 
-    # check when it is done
+    # check when job is done
     is_imputing = True
     while is_imputing:
         status = requests.get(url + f"/api/v2/jobs/{job_id.strip()}/status", headers=headers).json()
-        time.sleep(5) # check every 5 seconds
+        time.sleep(5) # check status every 5 seconds
         if status["complete"] == True and status["state"] == 5:
-            # it failed, need to re-send it
-	        #print("RE-SENT to impute")
-            #r = requests.post(url + api, files=files, data=data, headers=headers)
-		    #time.sleep(2)
-		    #job_id = r.json()["id"]
+		    # job failed, send exit code to Nextflow process to re-try it
             is_imputing = False
             exit(1)
         elif status["complete"] == True and status["state"] == 4:
+            # job completed successfully
             is_imputing = False
     time.sleep(4) # it will give time for folder structure to be recognized
     # if not nextflow will search for the path and will not exist
 
     # send chr,job_id to stdout so nextflow to catch it and save it to file
     subprocess.run(f"echo {job_id}", shell=True, check=True, stderr=subprocess.STDOUT)
-    #subprocess.run(f"echo {string_to_int}_{time_to_sleep}", shell=True, check=True, stderr=subprocess.STDOUT)
 
 if __name__ == "__main__":
     main()
--- workflows/QC_workflow.nf
+++ workflows/QC_workflow.nf
@@ -8,9 +8,11 @@ include { impute; extractZip; indexImputed } from '../modules/imputation.nf'
  * define the data analysis workflow
  */
 workflow process_vcf {
+
     // required inputs
     take:
         chroms_samples_ch
+
     // workflow implementation
     main:
         split_norm_phase(chroms_samples_ch)
@@ -19,17 +21,22 @@ workflow process_vcf {
                 split_norm_phase.out[2])
         extractZip( impute.out[0],
                     impute.out[1],
-                    impute.out[2].flatMap { n -> n.replaceAll(/\s+$/, "") })
+                    impute.out[2].flatMap { n -> n.replaceAll(/\s+$/, "") }) // replace whitespace
         indexImputed( extractZip.out.patient_name,
                         extractZip.out.chromosome )
 
+    // results
     emit:
         indexImputed.out.patient_name
 }
 
 workflow concatenate {
+
+    // normalized vcfs
     take:
         patient_dir_w_norm_vcfs
+
+    // perform task
     main:
         chr_concat( patient_dir_w_norm_vcfs )
 }
